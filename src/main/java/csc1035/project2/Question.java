781d1f1db1144ee8e9843a609015eca43b8e3d29
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-02-28 12:30:47 +0000

    Added, Getter, setter, and Constructor

--- src/main/java/csc1035/project2/Question.java
+++ src/main/java/csc1035/project2/Question.java
@@ -1,4 +1,37 @@
 package csc1035.project2;
 
 public class Question {
+    String question, category, answer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Question(String question, String category, String answer) {
+        this.question = question;
+        this.category = category;
+        this.answer = answer;
+
+
+    }
 }
