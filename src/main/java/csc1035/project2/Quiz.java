50f5dac9112e5dc83acab2b25cdc4ea24f9154b4
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-02-28 16:30:18 +0000

    Added getter, setter and constructor

--- src/main/java/csc1035/project2/Quiz.java
+++ src/main/java/csc1035/project2/Quiz.java
@@ -1,4 +1,56 @@
 package csc1035.project2;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+
+
+
 public class Quiz {
+
+    private String topic;
+
+    private String name;
+
+    private String difficulty;
+
+    private ArrayList<Question> questions;
+
+    public Quiz(String topic, ArrayList<Question> questions, String name, String difficulty) {
+        this.topic = topic;
+        this.questions = questions;
+        this.name = name;
+        this.difficulty = difficulty;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<Question> questions) {
+        this.questions = questions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
 }
