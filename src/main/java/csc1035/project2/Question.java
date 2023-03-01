dcda1e36e5d326c0c2337031b1a9e96dd8775faf
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-03-01 14:34:57 +0000

    Added auto increment for ID's

--- src/main/java/csc1035/project2/Question.java
+++ src/main/java/csc1035/project2/Question.java
@@ -1,57 +1,63 @@
-package csc1035.project2;
+    package csc1035.project2;
 
-import javax.persistence.*;
-@Entity
-@Table(name = "Questions")
-public class Question {
-    @Id
-    @Column
-    private int ID;
-    @Column
-    private String question;
-    @Column
-    private String category;
-    @Column
-    private String answer;
+    import javax.persistence.*;
+    @Entity
+    @Table(name = "Questions")
+    public class Question {
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column
+        private int ID;
+        @Column
+        private String question;
+        @Column
+        private String category;
+        @Column
+        private String answer;
 
-    @ManyToOne
-    @JoinColumn(nullable = false)
-    Quiz quiz;
-    public Question() {
+        @ManyToOne
+        @JoinColumn(nullable = false)
+        Quiz quiz;
+        public Question() {
 
+        }
+        public void setID(int ID) {
+            this.ID = ID;
+        }
+
+        public void setQuiz(Quiz quiz) {
+            this.quiz = quiz;
+        }
+
+        public String getQuestion() {
+            return question;
+        }
+
+        public void setQuestion(String question) {
+            this.question = question;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+
+        public String getAnswer() {
+            return answer;
+        }
+
+        public void setAnswer(String answer) {
+            this.answer = answer;
+        }
+
+        public Question(String question, String category, String answer) {
+            this.question = question;
+            this.category = category;
+            this.answer = answer;
+
+
+        }
     }
-
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public Question(int ID, String question, String category, String answer) {
-        this.question = question;
-        this.category = category;
-        this.answer = answer;
-        this.ID = ID;
-
-
-    }
-}
