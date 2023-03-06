0568df70a0d9dcdbe7ad09b2d003d217d8f22351
GitHST <c2037912@ncl.ac.uk>
2023-03-06 18:18:26 +0000

    connected scanner to csv file if it exists

--- src/main/java/csc1035/project2/Testing.java
+++ src/main/java/csc1035/project2/Testing.java
@@ -1,4 +1,15 @@
 package csc1035.project2;
-
+import java.io.File;
+import java.util.Scanner;
 public class Testing {
+    public static void main(String[] args) {
+        try {
+            File quizzes = new File("questions.csv");
+            Scanner scan = new Scanner(quizzes);
+            System.out.println("done");
+            System.out.println(scan);
+        } catch (java.io.FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
 }
