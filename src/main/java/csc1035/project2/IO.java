9a21c0c965c37106b53b10412f79a5eb8815bbaa
Ritvik04 <ritzverma04@gmail.com>
2023-03-06 18:17:55 +0000

    Progress on addQuestion method in QuestionStore and added the method to the IO class

--- src/main/java/csc1035/project2/IO.java
+++ src/main/java/csc1035/project2/IO.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class IO {
 
+    //
     public static void main(String[] args) {
         new IO().IOSystem();
 
--- src/main/java/csc1035/project2/QuestionStore.java
+++ src/main/java/csc1035/project2/QuestionStore.java
@@ -1,13 +1,59 @@
 package csc1035.project2;
 
+import org.hibernate.Session;
+
+import javax.persistence.Query;
+import java.util.List;
+import java.util.Scanner;
+
 public class QuestionStore {
-    public static void main(String[] args) {
+
+    // addQuestion(question, category, answer, quizID)
+    public void addQuestion() {
+        IO io = new IO();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nEnter the question: ");
+        String question = sc.nextLine();
+
+        System.out.println("\nEnter the category: ");
+        String category = sc.nextLine();
+
+        System.out.println("\nEnter the answer: ");
+        String answer = sc.nextLine();
+
+        System.out.println("\nEnter which of the following quizID's you would like to link this question with: ");
+        int quizID = sc.nextInt();
+
+        // Check if quizID exists in the database
+        Query query = session.createQuery("FROM Quiz WHERE questions = :quizID");
+        query.setParameter("quizID", quizID);
+        List<Quiz> quizList = query.getResultList();
+
+        if (quizList.isEmpty()) {
+            System.out.println("\nError: Quiz with quizID " + quizID + " does not exist.");
+            session.close();
+            io.IOSystem();
+        } else {
+            Question Q = new Question(question, category, answer, quizID);
+
+            session.save(Q);
+            session.getTransaction().commit();
+            System.out.println("\nQuestion added successfully to quiz " + quizID + ".");
+        }
+
+        session.close();
+    }
+
+    public static void deleteQuestion() {
 
     }
-    public static void addQuestion() {
+
+    public static void updateQuestion() {
 
     }
-
-    public static void deleteQuestion() {}
 
 }
