24135d62c305d89e351ac3fe769c95d7f7f1f65e
Ritvik04 <ritzverma04@gmail.com>
2023-03-06 17:25:15 +0000

    QuestionStore class added

--- src/main/java/csc1035/project2/QuestionStore.java
+++ src/main/java/csc1035/project2/QuestionStore.java
@@ -1,4 +1,13 @@
 package csc1035.project2;
 
 public class QuestionStore {
+    public static void main(String[] args) {
+
+    }
+    public static void addQuestion() {
+
+    }
+
+    public static void deleteQuestion() {}
+
 }
