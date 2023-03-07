1a9a19fa314f492a05c068a40a15f4f7ab3cf3fc
Ritvik04 <ritzverma04@gmail.com>
2023-03-07 21:38:05 +0000

    Added try, catch for addSAQ and addMCQ methods in the QuestionStore class...

--- src/main/java/csc1035/project2/QuestionStore.java
+++ src/main/java/csc1035/project2/QuestionStore.java
@@ -12,118 +12,142 @@ public class QuestionStore {
     public void addSAQ() {
 
         IO IO = new IO();
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
 
-        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("\nEnter the question: ");
-        String question = sc.nextLine();
-        question = question.toLowerCase(); // such that the question entered converts to lower case characters
+            System.out.println("\nEnter the question: ");
+            String question = sc.nextLine();
+            question = question.toLowerCase(); // such that the question entered converts to lower case characters
 
-        // The category can only be Maths, Architecture, Databases and Programming
-        System.out.println("\nEnter the category: ");
-        String category = sc.nextLine();
+            // The category can only be Maths, Architecture, Databases and Programming
+            System.out.println("\nEnter the category: ");
+            String category = sc.nextLine();
 
-        System.out.println("\nEnter the answer: ");
-        String answer = sc.nextLine();
-        answer = answer.toLowerCase(); // such that the answer entered converts to lower case characters
+            System.out.println("\nEnter the answer: ");
+            String answer = sc.nextLine();
+            answer = answer.toLowerCase(); // such that the answer entered converts to lower case characters
 
-        System.out.println("\nEnter which of the following quizID's you would like to link this question with: ");
+            System.out.println("\nEnter which of the following quizID's you would like to link this question with: ");
 
-        // Get a list of existing quizIDs from the database (tale Quiz)
-        TypedQuery<Integer> query = session.createQuery("SELECT DISTINCT ID FROM Quiz ", Integer.class);
-        List<Integer> quizIDs = query.getResultList();
+            // Get a list of existing quizIDs from the database (tale Quiz)
+            TypedQuery<Integer> query = session.createQuery("SELECT DISTINCT ID FROM Quiz ", Integer.class);
+            List<Integer> quizIDs = query.getResultList();
 
-        // Print a list of existing quizIDs
-        System.out.println("\nExisting quiz IDs:");
-        for (Integer id : quizIDs) {
-            System.out.println(id);
-        }
+            // Print a list of existing quizIDs
+            System.out.println("\nExisting quiz IDs:");
+            for (Integer id : quizIDs) {
+                System.out.println(id);
+            }
 
-        // Asking the user to choose from the following quizID's
-        int quizID = sc.nextInt();
+            // Asking the user to choose from the following quizID's
+            int quizID = sc.nextInt();
 
-        // Check if quizID exists in the database (table Quiz)
-        TypedQuery<Quiz> query1 = session.createQuery("FROM Quiz WHERE ID = :ID", Quiz.class);
-        query1.setParameter("ID", quizID);
-        List<Quiz> quizList = query1.getResultList();
+            // Check if quizID exists in the database (table Quiz)
+            TypedQuery<Quiz> query1 = session.createQuery("FROM Quiz WHERE ID = :ID", Quiz.class);
+            query1.setParameter("ID", quizID);
+            List<Quiz> quizList = query1.getResultList();
 
-        if (quizList.isEmpty()) {
-            System.out.println("\nError: Quiz with quizID " + quizID + " does not exist.");
+            if (quizList.isEmpty()) {
+                System.out.println("\nError: Quiz with quizID " + quizID + " does not exist.");
+                session.close();
+                IO.IOSystem();
+            } else {
+                Question Q = new Question(question, category, answer, quizID);
+
+                session.save(Q);
+                session.getTransaction().commit();
+                System.out.println("\nQuestion added successfully to quizID: " + quizID);
+            }
             session.close();
-            IO.IOSystem();
-        } else {
-            Question Q = new Question(question, category, answer, quizID);
+        } catch (HibernateException e){
+            //if error roll back
+            if(session!=null) session.getTransaction().rollback();
+            e.printStackTrace();
 
-            session.save(Q);
-            session.getTransaction().commit();
-            System.out.println("\nQuestion added successfully to quizID: " + quizID);
+        }finally {
+            //Close session
+            assert session != null;
+            session.close();
         }
-        session.close();
     }
 
     public void addMCQ(){
 
         IO io = new IO();
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
 
-        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("\nEnter the question: ");
-        String question = sc.nextLine();
-        question = question.toLowerCase(); // such that the question entered converts to lower case characters
+            System.out.println("\nEnter the question: ");
+            String question = sc.nextLine();
+            question = question.toLowerCase(); // such that the question entered converts to lower case characters
 
-        System.out.println("\nEnter the category: ");
-        String category = sc.nextLine();
+            System.out.println("\nEnter the category: ");
+            String category = sc.nextLine();
 
-        System.out.println("Enter possible answer");
-        String answer1 = sc.nextLine();
+            System.out.println("Enter possible answer");
+            String answer1 = sc.nextLine();
 
-        System.out.println("Enter the next possible answer");
-        String answer2 = sc.nextLine();
+            System.out.println("Enter the next possible answer");
+            String answer2 = sc.nextLine();
 
-        System.out.println("Enter the final possible answer");
-        String answer3 = sc.nextLine();
+            System.out.println("Enter the final possible answer");
+            String answer3 = sc.nextLine();
 
-        System.out.println("\nEnter the actual answer: ");
-        String actualAnswer = sc.nextLine();
-        actualAnswer = actualAnswer.toLowerCase(); // such that the answer entered converts to lower case characters
+            System.out.println("\nEnter the actual answer: ");
+            String actualAnswer = sc.nextLine();
+            actualAnswer = actualAnswer.toLowerCase(); // such that the answer entered converts to lower case characters
 
-        System.out.println("\nEnter which of the following quizID's you would like to link this question with: ");
+            System.out.println("\nEnter which of the following quizID's you would like to link this question with: ");
 
-        // Get a list of existing quizIDs from the database
-        TypedQuery<Integer> query = session.createQuery("SELECT DISTINCT  ID FROM Quiz ", Integer.class);
-        List<Integer> quizIDs = query.getResultList();
+            // Get a list of existing quizIDs from the database
+            TypedQuery<Integer> query = session.createQuery("SELECT DISTINCT  ID FROM Quiz ", Integer.class);
+            List<Integer> quizIDs = query.getResultList();
 
-        // Print a list of existing quizIDs
-        System.out.println("\nExisting quiz IDs:");
-        for (Integer id : quizIDs) {
-            System.out.println(id);
-        }
+            // Print a list of existing quizIDs
+            System.out.println("\nExisting quiz IDs:");
+            for (Integer id : quizIDs) {
+                System.out.println(id);
+            }
 
-        // Asking the user to choose from the following quizID's
-        int quizID = sc.nextInt();
+            // Asking the user to choose from the following quizID's
+            int quizID = sc.nextInt();
 
-        // Check if quizID exists in the database
-        TypedQuery<Question> query1 = session.createQuery("FROM Question WHERE quiz_id = :quiz_ID", Question.class);
-        query1.setParameter("quiz_ID", quizID);
-        List<Question> quizList = query1.getResultList();
+            // Check if quizID exists in the database
+            TypedQuery<Question> query1 = session.createQuery("FROM Question WHERE quiz_id = :quiz_ID", Question.class);
+            query1.setParameter("quiz_ID", quizID);
+            List<Question> quizList = query1.getResultList();
 
-        if (quizList.isEmpty()) {
-            System.out.println("\nError: Quiz with quizID " + quizID + " does not exist.");
+            if (quizList.isEmpty()) {
+                System.out.println("\nError: Quiz with quizID " + quizID + " does not exist.");
+                session.close();
+                io.IOSystem();
+            } else {
+                MCQ Q = new MCQ(question, category, answer1, answer2, answer3, actualAnswer, quizID);
+
+                session.save(Q);
+                session.getTransaction().commit();
+                System.out.println("\nQuestion added successfully to quizID: " + quizID);
+            }
             session.close();
-            io.IOSystem();
-        } else {
-            MCQ Q = new MCQ(question, category, answer1, answer2, answer3, actualAnswer, quizID);
 
-            session.save(Q);
-            session.getTransaction().commit();
-            System.out.println("\nQuestion added successfully to quizID: " + quizID);
+        } catch (HibernateException e){
+        //if error roll back
+        if(session!=null) session.getTransaction().rollback();
+        e.printStackTrace();
+
+        } finally {
+            //Close session
+            assert session != null;
+            session.close();
         }
-        session.close();
-
     }
     public void deleteSAQ() {
         IO io = new IO();
@@ -244,10 +268,12 @@ public class QuestionStore {
             // Commit the transaction and close the session
             session.getTransaction().commit();
             session.close();
+
         } catch (HibernateException e){
             //if error roll back
             if(session!=null) session.getTransaction().rollback();
             e.printStackTrace();
+
         }finally {
             //Close session
             assert session != null;
