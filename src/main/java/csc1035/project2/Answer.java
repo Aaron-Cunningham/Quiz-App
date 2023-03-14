870ed9fc695f52094d9869207910fbfc6d6cbed4
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-03-14 12:18:38 +0000

    Added answerSCQ method

--- src/main/java/csc1035/project2/Answer.java
+++ src/main/java/csc1035/project2/Answer.java
@@ -4,9 +4,16 @@ import org.hibernate.Session;
 
 import javax.persistence.Query;
 import java.util.List;
+import java.util.Scanner;
 
 public class Answer {
+
+    /**
+     * This method uses a for each loop to iterate through each question and then asks the user to answer it.
+     * If the answer matches the user input they score a point.
+     */
     public void answerSCQ(){
+        Scanner sc = new Scanner(System.in);
         Session session = HibernateUtil.getSessionFactory().openSession();
         session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
@@ -20,6 +27,21 @@ public class Answer {
         for (Object[] q: quizzes) {
             System.out.println("ID: " + q[0] + "\tName: " + q[1]);
         }
+
+        int quizID = sc.nextInt();
+        sc.nextLine();
+        Quiz targetQuiz = session.get(Quiz.class, quizID);
+
+        int count = 0;
+        for (Question question : targetQuiz.getQuestions()){
+            System.out.println(question.getQuestion());
+            String userAnswer = sc.nextLine();
+            if(userAnswer.equalsIgnoreCase(question.getAnswer())){
+                System.out.println();
+                count++;
+            }
+        }
+        System.out.println("You scored " + count);
 
 
 
