cd613f1a98a8e689a10ba21dbf5d36b54411e273
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-03-17 17:05:26 +0000

    Removed toString method

--- src/main/java/csc1035/project2/Question.java
+++ src/main/java/csc1035/project2/Question.java
@@ -76,17 +76,7 @@ public class Question {
         this.answer = answer;
     }
 
-    @Override
-    public String toString() {
-        return "Question{" +
-                "ID=" + ID +
-                ", question='" + question + '\'' +
-                ", category='" + category + '\'' +
-                ", answer='" + answer + '\'' +
-                ", quiz_id=" + quiz_id +
-                ", quiz=" + quiz +
-                '}';
-    }
+
 
     public Question(String question, String category, String answer, int quiz_id) {
         this.question = question;
