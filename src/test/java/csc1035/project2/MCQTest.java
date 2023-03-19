d32090f4b5555c3184a47af73b4779a8348c0d65
Josh <c2043603@ncl.ac.uk>
2023-03-19 20:35:28 +0000

    Added MCQTest Class with MCQ variable and setUp Method

---
+++ src/test/java/csc1035/project2/MCQTest.java
@@ -0,0 +1,88 @@
+package csc1035.project2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MCQTest {
+
+    //declaring private MCQ variable for testing
+    private MCQ testMCQ;
+
+    @BeforeEach
+    void setUp() {
+        testMCQ = new MCQ("What is 10+10?", "Fundamentals of Computing", "5", "10", "25", "20", 1);
+    }
+
+    @Test
+    void getID() {
+    }
+
+    @Test
+    void setID() {
+    }
+
+    @Test
+    void getQuestion() {
+    }
+
+    @Test
+    void setQuestion() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void setCategory() {
+    }
+
+    @Test
+    void getAnswer1() {
+    }
+
+    @Test
+    void setAnswer1() {
+    }
+
+    @Test
+    void getAnswer2() {
+    }
+
+    @Test
+    void setAnswer2() {
+    }
+
+    @Test
+    void getAnswer3() {
+    }
+
+    @Test
+    void setAnswer3() {
+    }
+
+    @Test
+    void getActualAnswer() {
+    }
+
+    @Test
+    void setActualAnswer() {
+    }
+
+    @Test
+    void getQuiz_id() {
+    }
+
+    @Test
+    void setQuiz_id() {
+    }
+
+    @Test
+    void getQuiz() {
+    }
+
+    @Test
+    void setQuiz() {
+    }
+}
