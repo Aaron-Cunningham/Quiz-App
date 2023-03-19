78880589a54f972957997ac57e412c51f18c833b
Josh <c2043603@ncl.ac.uk>
2023-03-19 21:47:33 +0000

    Added QuestionStoreTest Class

---
+++ src/test/java/csc1035/project2/QuestionStoreTest.java
@@ -0,0 +1,37 @@
+package csc1035.project2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionStoreTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addSAQ() {
+    }
+
+    @Test
+    void addMCQ() {
+    }
+
+    @Test
+    void deleteSAQ() {
+    }
+
+    @Test
+    void deleteMCQ() {
+    }
+
+    @Test
+    void updateSAQ() {
+    }
+
+    @Test
+    void updateMCQ() {
+    }
+}
