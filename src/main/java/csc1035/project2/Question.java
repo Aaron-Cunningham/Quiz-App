3f128855e2370e6cccb7025cd48bbc61c97a7e63
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-03-23 15:21:43 +0000

    Completed JavaDoc and comments for Question class

--- src/main/java/csc1035/project2/Question.java
+++ src/main/java/csc1035/project2/Question.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "Questions")
 public class Question {
+    //Creates columns for the questions table in the database
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column
@@ -19,6 +20,7 @@ public class Question {
     @Column
     private int quiz_id;
 
+    //Joins the Questions to a quiz via the quiz_id
     @ManyToOne
     @JoinColumn(name = "quiz_id", insertable = false, updatable = false)
     Quiz quiz;
@@ -31,6 +33,10 @@ public class Question {
         return quiz;
     }
 
+    /**
+     * This method sets the quiz ID to q question
+     * @param quiz_id
+     */
     public void setQuiz_id(int quiz_id) {
         this.quiz_id = quiz_id;
     }
@@ -39,45 +45,77 @@ public class Question {
         return ID;
     }
 
+
+    /**
+     * Gets the quiz ID the question is related to
+     * @return quiz_id
+     */
     public int getQuiz_id() {
         return quiz_id;
     }
 
-    public void setID(int ID) {
-        this.ID = ID;
-    }
 
     public void setQuiz(Quiz quiz) {
         this.quiz = quiz;
     }
 
+    /**
+     * gets the question from a question
+     * @return
+     */
     public String getQuestion() {
         return question;
     }
 
+    /**
+     * Sets the question for the question
+     * @param question
+     */
     public void setQuestion(String question) {
         this.question = question;
         question = question.toLowerCase();
     }
 
+    /**
+     * Gets the category of a question
+     * @return
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * sets the category of a question
+     * @param category
+     */
     public void setCategory(String category) {
         this.category = category;
     }
 
+    /**
+     * Gets the answer of a question
+     * @return answer
+     */
     public String getAnswer() {
         return answer;
     }
 
+    /**
+     * Sets the answer of a question
+     * @param answer
+     */
     public void setAnswer(String answer) {
         this.answer = answer;
     }
 
 
-
+    /**
+     * This construtor is representing a Question(SAQ)
+     * @param question - The actual question been asked
+     * @param category - The category ofn the question i.e maths, programming
+     * @param answer - The answer of the question
+     * @param quiz_id - This quiz ID the question is related to
+     */
     public Question(String question, String category, String answer, int quiz_id) {
         this.question = question;
         this.category = category;
