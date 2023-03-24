1c152b02ea780b249f3e01de7908089b7a7af106
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-03-24 11:47:09 +0000

    AddedJavaDoc

--- src/main/java/csc1035/project2/MCQ.java
+++ src/main/java/csc1035/project2/MCQ.java
@@ -52,58 +52,114 @@ public class MCQ {
         this.quiz_id = quiz_id;
     }
 
+    /**
+     * This method returns the ID of the quiz
+     * @return ID
+     */
     public int getID() {
         return ID;
     }
 
+    /**
+     * This method sets the ID of the quiz
+     * @param ID
+     */
     public void setID(int ID) {
         this.ID = ID;
     }
 
+    /**
+     * This method gets the question
+     * @return question
+     */
     public String getQuestion() {
         return question;
     }
 
+    /**
+     * This method sets the question
+     * @param question
+     */
     public void setQuestion(String question) {
         this.question = question;
     }
 
+    /**
+     * This method gets the category of a question
+     * @return category
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * This method sets the category of a quesiton
+     * @param category
+     */
     public void setCategory(String category) {
         this.category = category;
     }
 
+    /**
+     * This method gets a possible answer
+     * @return answer1
+     */
     public String getAnswer1() {
         return answer1;
     }
 
+    /**
+     * This methods sets a possible answer
+     * @param answer1
+     */
     public void setAnswer1(String answer1) {
         this.answer1 = answer1;
     }
 
+    /**
+     * This method gets a second possible answer
+     * @return answer2
+     */
     public String getAnswer2() {
         return answer2;
     }
 
+    /**
+     * This method sets a possible answer
+     * @param answer2
+     */
     public void setAnswer2(String answer2) {
         this.answer2 = answer2;
     }
 
+    /**
+     * This method gets a 3rd possible answer
+     * @return
+     */
     public String getAnswer3() {
         return answer3;
     }
 
+    /**
+     * This method sets a 3rd possible answer
+     * @param answer3
+     */
     public void setAnswer3(String answer3) {
         this.answer3 = answer3;
     }
 
+    /**
+     * This method returns the Actual answer
+     * @return
+     */
     public String getActualAnswer() {
         return actualAnswer;
     }
 
+    /**
+     * This method sets the actual answer
+     * @param actualAnswer
+     */
     public void setActualAnswer(String actualAnswer) {
         this.actualAnswer = actualAnswer;
     }
@@ -131,7 +187,7 @@ public class MCQ {
 
     /**
      * This method prints out the MCQ in this following format
-     * @return
+     * @return question, category, actualAnswer
      */
     @Override
     public String toString() {
